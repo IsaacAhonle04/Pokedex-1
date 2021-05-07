21821c5158897ba27a6c3fe562dc3a243cf99479
Isaac Ahonle <>
2021-05-07 23:24:06 +0000

    Consolidated all stat pulls into one request function

--- script.js
+++ script.js
@@ -5,32 +5,6 @@ function poke_random() {
   var poke_link = `https://pokeapi.co/api/v2/pokemon/${num}`
   //console.log(`Link: ${poke_link}`)
 
-  var name_request = new XMLHttpRequest()
-
-  name_request.open('GET', poke_link, true)
-
-  name_request.onload = function () {
-
-    var data = JSON.parse(this.response)
-    poke_name = data.forms[0].name
-    //console.log(`Name: ${poke_name}`)
-    document.getElementById("name").innerHTML = `Name: ${poke_name}`;
-  }
-
-  name_request.send()
-
- var species_page = new XMLHttpRequest()
-
-  species_page.open('GET', poke_link, true)
-
-  species_page.onload = function () {
-
-    var data = JSON.parse(this.response)
-    poke_species = data.species.url
-    console.log(`Species Link: ${poke_species}`)
-  }
-
-  species_page.send()
 
   var sprite = new XMLHttpRequest()
 
@@ -47,86 +21,41 @@ function poke_random() {
   sprite.send()
 
 
-  var hp_request = new XMLHttpRequest()
+  var poke_data_request = new XMLHttpRequest()
 
-  hp_request.open('GET', poke_link, true)
+  poke_data_request.open('GET', poke_link, true)
 
-  hp_request.onload = function () {
+  poke_data_request.onload = function () {
+
+    var data = JSON.parse(this.response)
+    poke_name = data.forms[0].name
+    //console.log(`Name: ${poke_name}`)
+    document.getElementById("name").innerHTML = `Name: ${poke_name}`;
 
     var data = JSON.parse(this.response)
     poke_hp = data.stats[0].base_stat
     //console.log(`HP: ${poke_hp}`)
     document.getElementById("hp").innerHTML = `HP: ${poke_hp}`;
-  }
-
-  hp_request.send()
-
-
-  var atk_request = new XMLHttpRequest()
-
-  atk_request.open('GET', poke_link, true)
-
-  atk_request.onload = function () {
 
     var data = JSON.parse(this.response)
     poke_atk = data.stats[1].base_stat
     //console.log(`Attack: ${poke_atk}`)
     document.getElementById("atk").innerHTML = `Attack: ${poke_atk}`;
-  }
-
-  atk_request.send()
-
-
-  var def_request = new XMLHttpRequest()
-
-  def_request.open('GET', poke_link, true)
-
-  def_request.onload = function () {
 
     var data = JSON.parse(this.response)
     poke_def = data.stats[2].base_stat
     //console.log(`Defense: ${poke_def}`)
     document.getElementById("def").innerHTML = `Defense: ${poke_def}`;
-  }
-
-  def_request.send()
-
-
-  var sp_atk_request = new XMLHttpRequest()
-
-  sp_atk_request.open('GET', poke_link, true)
-
-  sp_atk_request.onload = function () {
 
     var data = JSON.parse(this.response)
     poke_sp_atk = data.stats[3].base_stat
     //console.log(`Special Attack: ${poke_sp_atk}`)
     document.getElementById("sp-atk").innerHTML = `Special Attack: ${poke_sp_atk}`;
-  }
-
-  sp_atk_request.send()
-
-
-  var sp_def_request = new XMLHttpRequest()
-
-  sp_def_request.open('GET', poke_link, true)
-
-  sp_def_request.onload = function () {
 
     var data = JSON.parse(this.response)
     poke_sp_def = data.stats[4].base_stat
     //console.log(`Special Defense: ${poke_def}`)
     document.getElementById("sp-def").innerHTML = `Special Defense: ${poke_sp_def}`;
-  }
-
-  sp_def_request.send()
-
-
-  var spd_request = new XMLHttpRequest()
-
-  spd_request.open('GET', poke_link, true)
-
-  spd_request.onload = function () {
 
     var data = JSON.parse(this.response)
     poke_spd = data.stats[5].base_stat
@@ -134,6 +63,20 @@ function poke_random() {
     document.getElementById("spd").innerHTML = `Speed: ${poke_spd}`;
   }
 
-  spd_request.send()
+ var species_page = new XMLHttpRequest()
+
+  species_page.open('GET', poke_link, true)
+
+  species_page.onload = function () {
+
+    var data = JSON.parse(this.response)
+    poke_species = data.species.url
+    console.log(`Species Link: ${poke_species}`)
+  }
+
+  species_page.send()
+
+
+  poke_data_request.send()
 }
 poke_random()
