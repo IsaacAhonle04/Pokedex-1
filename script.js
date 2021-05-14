990715564a0d7d976592d785e81afcbda5e0cce2
Isaac Ahonle <>
2021-05-14 23:24:36 +0000

    Moved the buttons so that they are displayed inline under the data.

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
 function poke_all(link){  
-  var sprite = new XMLHttpRequest()
-
-  sprite.open('GET', link, true)
-
-  sprite.onload = function () {
-
-    var data = JSON.parse(this.response)
-    poke_sprite = data.sprites.front_default
-    //console.log(`Sprite Link: ${poke_sprite}`)
-    document.getElementById("sprite").src = poke_sprite;
-  }
-
-  sprite.send()
-
 
   var poke_data_request = new XMLHttpRequest()
 
@@ -26,38 +12,50 @@ function poke_all(link){
     //console.log(`Name: ${poke_name}`)
     document.getElementById("name").innerHTML = `Name: ${poke_name}`;
 
-    var data = JSON.parse(this.response)
+    poke_sprite = data.sprites.front_default
+    //console.log(`Sprite Link: ${poke_sprite}`)
+    document.getElementById("sprite").src = poke_sprite;
+
     poke_hp = data.stats[0].base_stat
     //console.log(`HP: ${poke_hp}`)
     document.getElementById("hp").innerHTML = `HP: ${poke_hp}`;
 
-    var data = JSON.parse(this.response)
     poke_atk = data.stats[1].base_stat
     //console.log(`Attack: ${poke_atk}`)
     document.getElementById("atk").innerHTML = `Attack: ${poke_atk}`;
 
-    var data = JSON.parse(this.response)
     poke_def = data.stats[2].base_stat
     //console.log(`Defense: ${poke_def}`)
     document.getElementById("def").innerHTML = `Defense: ${poke_def}`;
 
-    var data = JSON.parse(this.response)
     poke_sp_atk = data.stats[3].base_stat
     //console.log(`Special Attack: ${poke_sp_atk}`)
     document.getElementById("sp-atk").innerHTML = `Special Attack: ${poke_sp_atk}`;
 
-    var data = JSON.parse(this.response)
     poke_sp_def = data.stats[4].base_stat
     //console.log(`Special Defense: ${poke_def}`)
     document.getElementById("sp-def").innerHTML = `Special Defense: ${poke_sp_def}`;
 
-    var data = JSON.parse(this.response)
     poke_spd = data.stats[5].base_stat
     //console.log(`Speed: ${poke_spd}`)
     document.getElementById("spd").innerHTML = `Speed: ${poke_spd}`;
+
+    poke_species = data.species.url
+
+    var evo_page =  new XMLHttpRequest()
+    evo_page.open('GET', poke_species, true)
+    evo_page.onload = function (){
+      var data = JSON.parse(this.response)
+      poke_evo = data.evolution_chain.url
+      
+      var evolutions_request = new XMLHttpRequest()
+      evolutions_request.open('GET', poke_evo,true)
+
+    }
+    evo_page.send()    
   }
 
-  var species_page = new XMLHttpRequest()
+  /*var species_page = new XMLHttpRequest()
 
   species_page.open('GET', link, true)
 
@@ -74,17 +72,17 @@ function poke_all(link){
       //console.log(`Evolutions: ${poke_evo}`)
     }
     evo_page.send()
-  }
+  }*/
 
-  species_page.send()
   
 
   poke_data_request.send()
 }
 
+var num = ""
 
 function poke_random() {
-  var num = Math.floor(Math.random()*898)
+  num = Math.floor(Math.random()*898)
 
   var poke_link = `https://pokeapi.co/api/v2/pokemon/${num}`
   //console.log(`Link: ${poke_link}`)
@@ -93,6 +91,18 @@ function poke_random() {
 }
 
 poke_random()
+
+function poke_next(){
+  num = num+1
+  var poke_link_next = `https://pokeapi.co/api/v2/pokemon/${num}`
+  poke_all(poke_link_next)
+}
+
+function poke_prev(){
+  num = num-1
+  var poke_link_prev = `https://pokeapi.co/api/v2/pokemon/${num}`
+  poke_all(poke_link_prev)
+}
 
 
 function search(term_parameter){
